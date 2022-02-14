1ee8714bd12a3ecc1d8b4976665f3ef0dca489a7
Your Name7 <you@example.com>
2022-02-14 14:40:19 +0100

    ajout infos groupe

---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+#########################################
+# groupe MPCI 3
+# Célia GUILLOT
+# Bérénice GUEREL
+# Luka ZDRAVKOVIC
+# https://github.com/uvsq22106438/projet_tas_de_sable
+#########################################
+
