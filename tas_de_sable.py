370f72f1fa88c3a653ad8c11cf69c77da3564335
Your Name7 <you@example.com>
2022-02-14 15:17:18 +0100

    interface

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -13,6 +13,17 @@ import tkinter as tk
 
 #variables globales
 
+
+
 #fonctions
 
+
+#interface graphique
+
+root = tk.Tk()
+label = tk.label(root, text="", font=("courrier", "40"))
+label.grid(row=0, column=0)
+bouton = tk.Button(root, text="création de la configuration aléatoire", command=btn, font=("courrier", "30"))
+bouton.grid()
+root.mainloop()
 
